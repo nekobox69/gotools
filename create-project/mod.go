9cbf597b9c71bc27309ba833934afc2a72dba90e
nekobox69 <nekobox69@outlook.com>
2021-02-04 19:40:53 +0800

    feat: update zephyr

--- create-project/mod.go
+++ create-project/mod.go
@@ -17,7 +17,7 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/go-xorm/xorm v0.7.9
 	github.com/nekobox69/pocket v0.0.0-20210202124852-35d3e9068639
-	github.com/nekobox69/zephyr v0.0.0-20210128112727-f084f96824ae
+	github.com/nekobox69/zephyr v0.0.0-20210204113825-8986fd3e45e3
 	github.com/sirupsen/logrus v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 	xorm.io/core v0.7.3
